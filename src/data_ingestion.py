fc72c5db2fed9d3407e88d0293d51af716bc2500
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2026-03-25 22:58:43 +0100

    update modular data generation

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,25 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 # ----------------------------
 # 1. Data Ingestion & Simulation
-# ----------------------------
+# ---------------------------
+
 def generate_sensor_data(n_samples=1000, seq_len=50, n_sensors=3):
     data = np.random.normal(0, 1, (n_samples, seq_len, n_sensors))
     # Inject anomalies
-    for _ in range(int(0.05*n_samples)):
+    for _ in range(int(0.05 * n_samples)):
         idx = np.random.randint(0, n_samples)
         sensor_idx = np.random.randint(0, n_sensors)
         data[idx, :, sensor_idx] += np.random.normal(5, 1, seq_len)
     return data
 
-# Create labels (0 = normal, 1 = anomaly)
 def create_labels(data, threshold=3):
     labels = np.zeros(len(data))
     for i, sample in enumerate(data):
         if np.any(np.abs(sample) > threshold):
             labels[i] = 1
     return labels
-
-#X = generate_sensor_data()
-#y = create_labels(X)
