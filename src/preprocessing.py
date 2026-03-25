0390f79426a9754e3f35ac74571797b5181f2982
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2026-03-25 22:59:06 +0100

    update preprcoessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,18 +1,14 @@
-import utils
-import data_ingestion
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-X = data_ingestion.generate_sensor_data()
-y = data_ingestion.create_labels(X)
 # ----------------------------
 # 2. Data Preprocessing
 # ----------------------------
-# Flatten for classical ML
-X_flat = X.reshape(X.shape[0], -1)
-# Handle missing values (none in synthetic, but example)
-X_flat = np.nan_to_num(X_flat)
-# Normalize
-scaler = StandardScaler()
-X_flat = scaler.fit_transform(X_flat)
 
+def flatten_data(X):
+    return X.reshape(X.shape[0], -1)
+
+def normalize_data(X_flat):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_flat)
+    return X_scaled
