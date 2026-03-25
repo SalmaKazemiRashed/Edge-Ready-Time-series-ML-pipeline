3c9b2f31159b49c318e8933277cf1efd4b8d6be6
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2026-03-25 23:05:21 +0100

    visualize result

---
+++ src/visualization.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot_sample_series(X_sample):
+    plt.figure(figsize=(10,4))
+    for i in range(X_sample.shape[1]):
+        plt.plot(X_sample[:,i], label=f'Sensor {i}')
+    plt.title("Sample Sensor Time Series")
+    plt.legend()
+    plt.show()
